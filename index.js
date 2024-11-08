34d9b87775c43d90953a4df2394644a6d67371a1
chipsmelly <128431826+chipsmelly@users.noreply.github.com>
2024-11-08 12:39:57 -0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#legal-button").addEventListener("click", function () {
-    for (i = 0; i < document.querySelectorAll(".grid .cell-outer").length; i++) {
-        if (document.querySelectorAll(".grid .cell-outer")[i].classList.contains("legal") === false) {
-            document.querySelectorAll(".grid .cell-outer")[i].style.display = "none";
-        }
-    }
-} );
